bfa54c00816c64bf4a8c98142df983a46edf532e
Sergio Ramírez <sergioramirez@veryfi.com>
2023-03-11 21:05:11 -0500

    LP-359: Update Android Demo for W2

---
+++ app/src/main/java/com/veryfi/lens/w2/demo/logs/LogsViewHolder.kt
@@ -0,0 +1,15 @@
+package com.veryfi.lens.w2.demo.logs
+
+import androidx.recyclerview.widget.RecyclerView
+import com.veryfi.lens.w2.demo.databinding.LogsListItemBinding
+
+class LogsViewHolder(
+    private val itemBinding: LogsListItemBinding
+) : RecyclerView.ViewHolder(itemBinding.root) {
+
+    fun onBind(log: Log) {
+        itemBinding.logsTitle.text = log.title
+        itemBinding.logsMessage.setJson(log.message)
+        itemBinding.logsMessage.expandJson()
+    }
+}
